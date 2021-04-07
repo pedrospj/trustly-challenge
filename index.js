2b30ca7f170e74b39907e674321c76fd64304077
Pedro Pereira <p.pedrospj@gmail.com>
2021-04-07 19:57:58 -0300

    chore: initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.status(200).json({ hello: "world" });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`App is listening in port ${PORT}`);
+});
