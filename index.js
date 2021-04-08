581fbb7fcda68b8c97cc82bf51b19656b1dc6ed0
Pedro Pereira <p.pedrospj@gmail.com>
2021-04-07 21:34:43 -0300

    feat: get all links

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const routes = require('./src/constants/routes');
 
 const RepoController = require('./src/controllers/repoController');
 
 const app = express();
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: false }));
 
 //mounting routes
 const repoController = new RepoController();
--- src/controllers/repoController.js
+++ src/controllers/repoController.js
@@ -1,7 +1,11 @@
+const getFilesLinks = require('../helpers/getFilesLinks');
+
 class RepoControler {
   getRepoinfo = async (req, res) => {
+    const path = 'pedrospj/lista-de-videos/file-list/master';
+    const fileLinks = await getFilesLinks(path, res);
     res.status(200).json({
-      hello: 'world',
+      hello: fileLinks,
     });
   };
 }
