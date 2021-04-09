ddda2346f8530c5b436fa1a3b63cdbec77dfbef8
Pedro Pereira <p.pedrospj@gmail.com>
2021-04-09 16:38:43 -0300

    Test: msw test (#7)
    * feat: add msw
    
    * test: test helpers

--- src/__tests__/repoController.test.js
+++ src/__tests__/repoController.test.js
@@ -1,23 +1,49 @@
-const request = require('supertest');
+const { rest } = require('msw');
+const { setupServer } = require('msw/node');
+const getFilesLinks = require('../helpers/getFilesLinks');
+const processFileLink = require('../helpers/processFileLink');
+const { mockHtml, mockRawFile } = require('../constants/mock');
+const github = require('../constants/github');
+const REPO = `pedrospj/trustly-challenge/file-list/main`;
 
-const HEROKU_URL = 'https://pedrospj-trustly-challenge.herokuapp.com';
-const REPO = '/pedrospj/trustly-challenge';
+//mock github response
+const server = setupServer(
+  rest.get(`${github.GITHUB_URL}/*`, (req, res, ctx) => {
+    return res(ctx.status(200), ctx.body(mockHtml));
+  }),
 
-describe(`GET ${REPO}`, () => {
-  it('should return 200 and the info about the repo', (done) => {
-    request(HEROKU_URL)
-      .get(`${REPO}/main`)
-      .expect(200)
-      .then((resp) => {
-        const jsonInfo = resp.body.find((info) => info.extension === 'json');
-        expect(jsonInfo.count).toBe(2);
-        expect(jsonInfo.bytes).toBe(117114);
-        expect(jsonInfo.lines).toBe(3026);
-        done();
-      });
+  rest.get(`${github.GITHUB_RAW_FILE}/*`, (req, res, ctx) => {
+    return res(ctx.status(200), ctx.body(mockRawFile));
+  })
+);
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
+
+describe('getFilesLinks', () => {
+  it('should return files URLs', async () => {
+    const links = await getFilesLinks(REPO);
+
+    expect(links).toEqual([
+      'pedrospj/trustly-challenge/main/.eslintrc.js',
+      'pedrospj/trustly-challenge/main/.gitignore',
+      'pedrospj/trustly-challenge/main/.prettierrc',
+      'pedrospj/trustly-challenge/main/index.js',
+      'pedrospj/trustly-challenge/main/jest.config.js',
+      'pedrospj/trustly-challenge/main/package-lock.json',
+      'pedrospj/trustly-challenge/main/package.json',
+    ]);
   });
+});
 
-  it('should return 400 Bad Request', (done) => {
-    request(HEROKU_URL).get(`${REPO}/master`).expect(400, done);
+describe('getFilesLinks', () => {
+  it('should return files URLs', async () => {
+    const finalObj = {};
+    await processFileLink(finalObj, 'pedrospj/trustly-challenge/main/index.js');
+
+    expect(finalObj).toEqual({
+      js: { extension: 'js', bytes: 659, lines: 27, count: 1 },
+    });
   });
 });
